ff67debaca681826b6613f280c45dff38d1a787d
Fabio <payoartudi@hotmail.it>
2025-01-14 20:07:19 +0100

    aggiunto tendina lingue e funzionamento JS a file homepage

--- homepage.js
+++ homepage.js
@@ -1,16 +1,31 @@
 // bottone - hamburger-menu'
 
 const bottoneHamburger = document.querySelector("#bottone-hamburger");
-const body = document.querySelector("body");
+const showTendina = document.querySelector(".show-tendina");
 
 bottoneHamburger.addEventListener("click", function () {
-  body.classList.toggle("show-tendina");
+  showTendina.classList.toggle("show-tendina");
 });
 
 document
   .querySelector(".tendina")
   .addEventListener("click", function (clickTendina) {
     if (clickTendina.target === this) {
-      body.classList.remove("show-tendina");
+      showTendina.classList.remove("show-tendina");
+    }
+  });
+
+const bottoneLingue = document.querySelector(`.bottone-nav-lingua`);
+const showTendinaLingue = document.querySelector(`.show-tendina-lingue`);
+
+bottoneLingue.addEventListener(`click`, function () {
+  showTendinaLingue.classList.toggle(`show-tendina-lingue`);
+});
+
+document
+  .querySelector(".tendina-lingue")
+  .addEventListener("click", function (clickTendina) {
+    if (clickTendina.target === this) {
+      showTendinaLingue.classList.remove("show-tendina-lingue");
     }
   });
