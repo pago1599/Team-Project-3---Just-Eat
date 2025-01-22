d078ccaa3527564fabd530e35f4daf9bcf3b4cd9
Fabio <payoartudi@hotmail.it>
2025-01-22 16:37:24 +0100

    aggiunta funzion filtro e tendina a searchpage

--- search-page.js
+++ search-page.js
@@ -249,3 +249,45 @@ document.addEventListener("DOMContentLoaded", function () {
     mostraRistoranti(filtro);
   });
 });
+
+// bottone - hamburger-menu'
+
+const bottoneHamburger = document.querySelector("#bottone-hamburger");
+const showTendina = document.querySelector(".tendina-container");
+
+bottoneHamburger.addEventListener("click", function () {
+  showTendina.classList.toggle("show-tendina");
+});
+
+document
+  .querySelector(".tendina")
+  .addEventListener("click", function (clickTendina) {
+    if (clickTendina.target === this) {
+      showTendina.classList.remove("show-tendina");
+    }
+  });
+
+// script per chiudere la tendina hamburger menu' dalla x di dentro
+const chiudiBtn = document.getElementById("chiudi-tendina-hamburger");
+
+chiudiBtn.addEventListener("click", () => {
+  showTendina.classList.remove("show-tendina");
+});
+
+// bottone - lingue
+const bottoneLingue = document.querySelector(`.bottone-nav-lingua`);
+const showTendinaLingue = document.querySelector(`.tendina-lingue-container`);
+
+bottoneLingue.addEventListener(`click`, function () {
+  showTendinaLingue.classList.toggle(`show-tendina-lingue`);
+});
+
+document.addEventListener("click", function (event) {
+  // Controlla se il clic è avvenuto fuori dalla tendina e dal bottone
+  if (
+    !showTendinaLingue.contains(event.target) &&
+    !bottoneLingue.contains(event.target)
+  ) {
+    showTendinaLingue.classList.remove("show-tendina-lingue");
+  }
+});
